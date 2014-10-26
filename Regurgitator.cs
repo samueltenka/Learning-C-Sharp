48d8312d132e0e9adfee4b7a1518b5fb891f726e
samueltenka <iguavadon@gmail.com>
2014-10-26 13:35:03 -0700

    Create Regurgitator.cs

---
+++ Regurgitator.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_00 {
+    class Regurgitator {
+        public static void Main() {
+            while(true) {
+                String X = System.Console.ReadLine();
+                System.Console.WriteLine(X);
+            }
+        }
+    }
+}
